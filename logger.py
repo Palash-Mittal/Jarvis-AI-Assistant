0fd132007d98397798cfb1946608405220637ea8
Palash-Mittal <palashmittal6@gmail.com>
2026-01-21 20:12:56 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 import config
 import os
 
-LOG_PATH = config.ENGINE.get("log_path", "backend/jarvis.log")
+LOG_PATH = config.ENGINE.get("log_path", "jarvis.log")
 os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
 
 handler = RotatingFileHandler(LOG_PATH, maxBytes=2_000_000, backupCount=3, encoding="utf-8")
